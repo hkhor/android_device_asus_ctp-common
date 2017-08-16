3e1c6413d2c21a063861a8487958f7de4a3e514d
Khor Chin Hui <huikhor19990520@gmail.com>
2017-08-17 01:53:43 +0800

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -193,6 +193,7 @@ BOARD_CANT_BUILD_RECOVERY_FROM_BOOT_PATCH := true
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 TARGET_RECOVERY_DEVICE_MODULES := libinit_ctp librecovery_updater_ctp intel_prop thermald upi_ug31xx
+TARGET_RECOVERY_FSTAB := device/asus/T00F/rootdir/etc/fstab.redhookbay
 
 #TWRP
 TW_THEME := portrait_hdpi
@@ -205,3 +206,4 @@ BOARD_HAS_NO_REAL_SDCARD := true
 TW_INCLUDE_NTFS_3G := true
 BOARD_SUPPRESS_EMMC_WIPE := true
 RECOVERY_VARIANT := twrp
+PRODUCT_COPY_FILES += device/asus/T00F/twrp.fstab:recovery/root/etc/twrp.fstab
